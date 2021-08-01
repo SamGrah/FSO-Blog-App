75265582d0a67400e0aed6e2d8491ade263e8a45
SamGrah <segrah@gmail.com>
2021-07-31 21:48:41 -0400

    abstracted Blog model

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const Blog = require('./models/note')
+const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
