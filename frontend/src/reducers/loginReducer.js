13f29e65306ed03857c827d1d53bb609ef608056
Sam Graham <samgraham@Sams-MacBook-Air.local>
2021-08-07 08:11:41 -0400

    added redux store persists page refresh

--- frontend/src/reducers/loginReducer.js
+++ frontend/src/reducers/loginReducer.js
@@ -1,4 +1,10 @@
+const initializeLogin = () => { 
+  const currentUser = window.localStorage.getItem('loginSessionData')
+  return currentUser ? JSON.parse(currentUser) : {}
+}
+
 export const loginUser = (loginData) => {
+  window.localStorage.setItem('loginSessionData', JSON.stringify(loginData))
   return {
     type: 'LOGIN',
     data: loginData
@@ -6,12 +12,13 @@ export const loginUser = (loginData) => {
 }
 
 export const logoutUser = () => {
+  window.localStorage.removeItem('loginSessionData')
   return {
     type: 'LOGOUT'
   }
 }
 
-const loginReducer = (state = {}, action) => {
+const loginReducer = (state = initializeLogin(), action) => {
   switch(action.type) {
     case 'LOGIN':
       return action.data
